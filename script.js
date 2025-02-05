35c11f2d8daef676e449d83f31c3686b3164ef40
PrateekRajput12 <ficredinn@gmail.com>
2025-02-05 20:51:06 +0530

    Que 3

--- script.js
+++ script.js
@@ -9,8 +9,26 @@
 
 // Que 2
 
-
+// -------Static
 // let a = 5
 // let b = 10
 // let c = a + b
 // document.writeln(c)
+
+// ------ Dynamic
+
+// let a = parseInt(prompt("enter value of a"))
+// let b = parseInt(prompt("Enter value of b"))
+// let c = a + b
+// document.writeln(c)
+
+
+
+//  Que-3 Sqe root of number 
+
+let a = 8
+let sqr1 = a * a
+document.writeln("Sqr root of a is " + sqr1)
+let b = parseInt(prompt("enter value of b "))
+let sqr2 = b * b
+document.writeln("Sqr root of b is " + sqr2)
