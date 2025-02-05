bf97ea40cec54d155f5625280771f4184399657c
PrateekRajput12 <ficredinn@gmail.com>
2025-02-05 21:09:57 +0530

    Que 4

--- script.js
+++ script.js
@@ -26,9 +26,25 @@
 
 //  Que-3 Sqe root of number 
 
-let a = 8
-let sqr1 = a * a
-document.writeln("Sqr root of a is " + sqr1)
-let b = parseInt(prompt("enter value of b "))
-let sqr2 = b * b
-document.writeln("Sqr root of b is " + sqr2)
+// let a = 8
+// let sqr1 = a * a
+// document.writeln("Sqr root of a is " + sqr1)
+// let b = parseInt(prompt("enter value of b "))
+// let s5qr2 = b * b
+// document.writeln("Sqr root of b is " + sqr2)
+
+// Que-4 Area of traingle
+
+// 2
+// using heron's formula
+
+// let a = parseInt(prompt("Enter value of a : "))
+// let bb = parseInt(prompt("Enter value of b : "))
+// let c = parseInt(prompt("Enter value of c : "))
+// let s = (a + bb + c) / 2
+
+// document.write("s is :" + s)
+
+// let temp = s * (s - a) * (s - b) * (s - c)
+// let area = Math.sqrt(temp)
+// document.write("a is: " + area)
